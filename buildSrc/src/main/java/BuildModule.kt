aab13cb16f4bbc68a189d52c17bfde63d2ca1479
Samuel Dalafiari <dalafiarisamuel@gmail.com>
2022-05-26 20:25:20 +0100

    added BuildModule.kt file to buildSrc folder

---
+++ buildSrc/src/main/java/BuildModule.kt
@@ -0,0 +1,8 @@
+object BuildModule {
+    const val APP = ":app"
+    const val REMOTE = ":remote"
+    const val DOMAIN = ":domain"
+    const val CACHE = ":cache"
+    const val DATA = ":data"
+    const val CORE = ":core"
+}
