6c1716631cfcddd82653821f2ec0ba3ca9ef5a37
Samuel Dalafiari <dalafiarisamuel@gmail.com>
2022-05-27 16:16:29 +0100

    provided visibility for each module dependency and interconnection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ android {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(project(BuildModule.DOMAIN))
     implementation(project(BuildModule.CORE))
     implementAll(Dependencies.implementations)
